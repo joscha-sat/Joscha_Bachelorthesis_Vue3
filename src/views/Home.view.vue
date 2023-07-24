12574b3ce554c096b656f21efe3566d3ffd166b4
joscha-sat <joscha.sattler@web.de>
2023-07-25 00:24:30 +0200

    home view adjustments

--- src/views/Home.view.vue
+++ src/views/Home.view.vue
@@ -1,80 +1,85 @@
 <!-- TS ------------------------------------------------------------//-->
-<script lang="ts" setup>
+<script lang='ts' setup>
 import happyDog from '@/assets/images/dog/happyDog.png';
 import happyCat from '@/assets/images/cat/happyCat.png';
 import bark from '../assets/sounds/dog/bark.mp3';
 import meow from '../assets/sounds/cat/meow.mp3';
 
-import { useMascotStore } from "@/stores/mascot.store";
-import TitleWithSound from "@/components/shared/TitleWithSound.vue";
-import SelectTask from "@/components/main-menu/Select-Task.vue";
-import { storeToRefs } from "pinia";
-import { ref } from "vue";
+import { useMascotStore } from '@/stores/mascot.store';
+import TitleWithSound from '@/components/shared/TitleWithSound.vue';
+import SelectTask from '@/components/main-menu/Select-Task.vue';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 
 const audioRef = ref<HTMLAudioElement>();
 
 const mascotStore = useMascotStore();
-const { isDog } = storeToRefs(mascotStore)
+const { isDog } = storeToRefs(mascotStore);
 
 const playSound = () => {
-    audioRef.value.play()
-}
+    audioRef.value.play();
+};
 
 </script>
 
 <!-- HTML ----------------------------------------------------------//-->
 <template>
 
-    <TitleWithSound title="Hello and welcome to listen, play, learn! Choose a task"/>
+    <TitleWithSound title='Hello and welcome to listen, play, learn! Choose a task' />
 
-    <div class="container">
-        <div class="left"></div>
+    <div class='container'>
+        <div class='left'></div>
 
-        <div class="content">
-            <SelectTask/>
+        <div class='content'>
+            <SelectTask />
         </div>
 
-        <div class="right">
-            <img :src="isDog? happyDog : happyCat" alt="mascot" class="image" style="cursor: pointer"
-                 @click="playSound">
-            <audio ref="audioRef" :src="isDog? bark : meow"></audio>
+        <div class='right'>
+            <img :src='isDog? happyDog : happyCat' alt='mascot' class='image' style='cursor: pointer'
+                 @click='playSound'>
+            <audio ref='audioRef' :src='isDog? bark : meow'></audio>
         </div>
     </div>
 
 </template>
 
 <!-- SCSS ---------------------------------------------------------// -->
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 .container {
     display: flex;
-    justify-content: center;
+    place-content: center;
+    place-items: center;
     gap: 1rem;
     margin-top: 1.5rem;
 }
 
-.left {
-    flex-basis: 10%;
-}
 
 .content {
-    flex-basis: 70%;
     display: flex;
     justify-content: center;
-
+    margin-left: 15rem;
 }
 
 .right {
-    flex-basis: 20%;
     display: flex;
     justify-content: flex-end;
 }
 
 .image {
     height: 450px;
-
     object-fit: contain;
 }
+
+@media screen and (max-width: 1224px) {
+    .right {
+        display: none;
+    }
+    .content {
+        margin-left: 0;
+    }
+}
+
 </style>
 
 
-
+<!--1224px-->

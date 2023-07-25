116d05e32205fe22a7c78eff8f02847384c7ff1a
joscha-sat <joscha.sattler@web.de>
2023-07-25 06:48:22 +0200

    settings padding added to button

--- src/views/Settings.view.vue
+++ src/views/Settings.view.vue
@@ -1,52 +1,52 @@
 <!-- TS ------------------------------------------------------------//-->
-<script lang="ts" setup>
-import ColorThemePicker from "@/components/settings/Color-Theme-Picker.vue";
-import BackgroundMusicSettings from "@/components/settings/Background-Music-Settings.vue";
-import FontSizeSettings from "@/components/settings/Font-Size-Settings.vue";
-import FontFamilySettings from "@/components/settings/Font-Family-Settings.vue";
-import SaveCancelButtons from "@/components/shared/Save-Cancel-Buttons.vue";
+<script lang='ts' setup>
+import ColorThemePicker from '@/components/settings/Color-Theme-Picker.vue';
+import BackgroundMusicSettings from '@/components/settings/Background-Music-Settings.vue';
+import FontSizeSettings from '@/components/settings/Font-Size-Settings.vue';
+import FontFamilySettings from '@/components/settings/Font-Family-Settings.vue';
+import SaveCancelButtons from '@/components/shared/Save-Cancel-Buttons.vue';
 </script>
 
 <!-- HTML ----------------------------------------------------------//-->
 <template>
     <v-container>
 
-        <v-card class="card" color="bg">
+        <v-card class='card' color='bg'>
             <!-- TITLE -->
             <v-card-title>
                 Settings
             </v-card-title>
 
             <!-- CONTENT -->
-            <v-card-text class="cardText">
+            <v-card-text class='cardText'>
                 <!-- PRIMARY COLOR PICKER -->
-                <div class="block">
-                    <ColorThemePicker/>
+                <div class='block'>
+                    <ColorThemePicker />
                 </div>
 
                 <!-- BACKGROUND MUSIC CHECKBOX -->
-                <div class="block">
-                    <BackgroundMusicSettings/>
+                <div class='block'>
+                    <BackgroundMusicSettings />
                 </div>
 
                 <!-- FONT SIZE SETTER -->
-                <div class="block">
-                    <FontSizeSettings/>
+                <div class='block'>
+                    <FontSizeSettings />
                 </div>
 
                 <!-- FONT FAMILY SELECTOR -->
-                <div class="block">
-                    <FontFamilySettings/>
+                <div class='block'>
+                    <FontFamilySettings />
                 </div>
             </v-card-text>
 
-            <SaveCancelButtons/>
+            <SaveCancelButtons style='padding: 1rem' />
         </v-card>
     </v-container>
 </template>
 
 <!-- SCSS ---------------------------------------------------------// -->
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 .card {
     margin: auto;
     width: min(35rem, 100%);

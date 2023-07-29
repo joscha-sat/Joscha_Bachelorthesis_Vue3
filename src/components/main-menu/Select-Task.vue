17b92d2e27dd2a994c27e7094a17867bd0b31988
joscha-sat <joscha.sattler@web.de>
2023-07-29 04:51:09 +0200

    words highlighted

--- src/components/main-menu/Select-Task.vue
+++ src/components/main-menu/Select-Task.vue
@@ -1,6 +1,6 @@
 <!-- TS ------------------------------------------------------------//-->
-<script lang="ts" setup>
-import { useSpeechSynthesis } from "@vueuse/core";
+<script lang='ts' setup>
+import { useSpeechSynthesis } from '@vueuse/core';
 
 const speechStore = useTextToSpeechStore();
 const { speech, voice } = storeToRefs(speechStore);
@@ -10,11 +10,11 @@ import words from '../../assets/images/tasks/words.jpg';
 import iSound from '../../assets/images/tasks/initialSound.png';
 import syllables from '../../assets/images/tasks/syllables.jpg';
 import phonemes from '../../assets/images/tasks/phonemes.png';
-import { useTextToSpeechStore } from "@/stores/TextToSpeech.store";
-import { storeToRefs } from "pinia";
-import { ref } from "vue";
-import ImageCard from "@/components/shared/Image-Card.vue";
-import router from "@/router";
+import { useTextToSpeechStore } from '@/stores/TextToSpeech.store';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import ImageCard from '@/components/shared/Image-Card.vue';
+import router from '@/router';
 
 const tasks = ref([
     { i18n: 'Listening', src: listening, to: 'listening' },
@@ -22,44 +22,44 @@ const tasks = ref([
     { i18n: 'Sentences and words', src: words, to: 'sentences_and_words' },
     { i18n: 'Syllables', src: syllables, to: 'syllables' },
     { i18n: 'Initial sound', src: iSound, to: 'initial_sound' },
-    { i18n: 'Phonemes', src: phonemes, to: 'phonemes' }
+    { i18n: 'Phonemes', src: phonemes, to: 'phonemes' },
 ]);
 
 const playVoice = (txt: string) => {
     speech.value = useSpeechSynthesis(txt, {
-        voice
-    })
-    speech.value.speak()
-}
+        voice,
+    });
+    speech.value.speak();
+};
 
 const toTask = (to: string) => {
 
     router.push({
-        path: to
-    })
+        path: to,
+    });
 
-}
+};
 
 </script>
 
 
 <!-- HTML ----------------------------------------------------------//-->
 <template>
-    <div class="grid">
+    <div class='grid'>
         <ImageCard
-          v-for="(task, i) in tasks"
-          :key="i"
-          :src="task.src"
-          :title="task.i18n"
-          @clickImg="toTask(task.to)"
-          @voice="playVoice(task.i18n)"
+            v-for='(task, i) in tasks'
+            :key='i'
+            :src='task.src'
+            :title='task.i18n'
+            @clickImg='toTask(task.to)'
+            @voice='playVoice(task.i18n)'
         />
     </div>
 
 </template>
 
 <!-- SCSS ---------------------------------------------------------// -->
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 .grid {
     display: flex;
     gap: 1.5rem;
